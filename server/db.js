e6b6a7d59f836c0e94a9ca6708020c7824ffbe1f
sairam <sairamvakkalagadda@gmail.com>
2025-03-07 17:57:53 +0530

    changed the db credetials

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 // Create a connection to the MySQL database
 const connection = mysql.createPool({
-    host: 'bitcot-database.ck7yas6iqahb.us-east-1.rds.amazonaws.com',  // Local machine IP address or localhost
-    user: 'admin',       // MySQL username (default: 'root')
-    password: 'Srikanth@aws59',       // MySQL password (leave empty if default)
+    host: 'bitcot.ck7yas6iqahb.us-east-1.rds.amazonaws.com',  // Local machine IP address or localhost
+    user: 'Genesis',       // MySQL username (default: 'root')
+    password: 'Genesis123',       // MySQL password (leave empty if default)
     database: 'Bitcot', // Replace with your actual database name
     port: 3306 ,         // Default MySQL port
     waitForConnections: true,
